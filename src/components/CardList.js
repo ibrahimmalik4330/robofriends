30afb6ec491928c7f24cbaf9fa41c980df93ffcc
Ibrahim <ibrahimmalik4330@gmail.com>
2025-05-25 13:00:45 +0500

    removes scroll from CardList & add border

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Card from "./Card";
 
-const CardList = ({robots}) => {
-    return (
-        <div>
-    {robots.map((user, i) => {
+const CardList = ({ robots }) => {
+  return (
+    <div
+      style={{
+        padding: "20px 0",
+        border: "3px solid #000000",
+        marginTop: "10px",
+      }}
+    >
+      {robots.map((user, i) => {
         return (
-         <Card 
-            key={user.id} 
-            id={robots[i].id} 
-            name={robots[i].name} 
-            email={robots[i].email} 
-         />);
-    })}
+          <Card
+            key={user.id}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
-    );
-}
+  );
+};
 
 export default CardList;
